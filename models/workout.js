95e602911c6898d2957f24baa97528ce2bf2c339
Montii <llh9@yahoo.com>
2021-12-04 20:02:28 -0500

    created an index.js in models

--- models/workout.js
+++ models/workout.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  exercises: {
-    type: Object
-  }
+  exercise: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Book"
+    }
+  ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
